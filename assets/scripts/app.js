101d7a116d75e1643e8efd17c66c8ad6d9be36ae
lukejcn <luke.jones@carenav.co.uk>
2021-10-23 14:14:33 +0100

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,24 @@
+const ATTACK_VALUE = 10
+const MONSTER_ATTACK_VALUE = 14
+
+let chosenMaxLife = 100
+let currentMonsterHealth = chosenMaxLife
+let currentPlayerHealth = chosenMaxLife
+
+adjustHealthBars(chosenMaxLife)
+
+function attackHandler() {
+    const damage = dealMonsterDamage(ATTACK_VALUE)
+    currentMonsterHealth -= damage
+    const playerdamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentPlayerHealth -= playerdamage
+    if (currentMonsterHealth <= 0) {
+        alert('You Won')
+    }
+    if (currentPlayerHealth <= 0) {
+        alert('You lose :(')
+    }
+}
+
+
+attackBtn.addEventListener('click', attackHandler)
