49fd69032b886218a757d9af6a26da97e18e24cc
lukejcn <luke.jones@carenav.co.uk>
2021-10-23 16:06:43 +0100

    Add Heal Function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10
 const STRONG_ATTACK_VALUE = 17
 const MONSTER_ATTACK_VALUE = 14
+const HEAL_VALUE = 20
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -39,5 +40,12 @@ function strongAttackHandler() {
     attackMonster('STRONG_ATTACK')
 }
 
+function healPlayerHandler() {
+     increasePlayerHealth(HEAL_VALUE)
+     currentPlayerHealth += HEAL_VALUE
+     endRound()
+}
+
 attackBtn.addEventListener('click', attackHandler)
 strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
