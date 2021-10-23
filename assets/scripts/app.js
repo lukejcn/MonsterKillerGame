8e9a5a6b4d76080b2ad9d637d1eb628d8b4c3632
lukejcn <luke.jones@carenav.co.uk>
2021-10-23 14:31:04 +0100

    add strong attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10
+const STRONG_ATTACK_VALUE = 17
 const MONSTER_ATTACK_VALUE = 14
 
 let chosenMaxLife = 100
@@ -21,5 +22,19 @@ function attackHandler() {
     }
 }
 
+function strongAttackHandler() {
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE)
+    currentMonsterHealth -= damage
+    const playerdamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentPlayerHealth -= playerdamage
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You Won')
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lose :(')
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert('draw')
+    }
+}
 
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler)
